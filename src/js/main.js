8bc95d140bb131992f3ed5722887dd9f64ef468b
Isac Petinate <ipetinate@viceri.com.br>
2019-06-26 17:16:40 -0300

    Exemplo de callback

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,35 @@
-export default () => {
+// exemplo de implementcao do padrao callback
 
-}
+function calculate(n1, callback){
+    let n2 = n1 + 1
 
-function montarLinha () {
+    let err = ``;
+    if(n1 == 0)
+        err ="numero errado parca"; 
     
+   let result = callback(n1,n2, err);
+
+   if(!err) {
+    console.log(`o resultado e ${result}`)
+    }
 }
+
+calculate(0, function(number1, number2, err){
+    if(err) console.log(err);
+    return number1 + number2;
+});
+
+calculate(1, function(number1, number2, err){
+    if(err) console.log(err);
+    return number1 - number2;
+});
+
+calculate(1, function(number1, number2, err){
+    if(err) console.log(err);
+    return number1 * number2;
+});
+
+calculate(1, function(number1, number2, err){
+    if(err) console.log(err);
+    return number1 / number2;
+});
