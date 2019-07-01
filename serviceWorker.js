92efe81e1bf202a411676bb84b59bea7011307c5
Isac Petinate <idpetinate@gmail.com>
2019-07-01 01:21:31 -0300

    Add Service Worker (from G Devs)

---
+++ serviceWorker.js
@@ -0,0 +1,60 @@
+var CACHE_NAME = 'vanilla-cache';
+var urlsToCache = [
+  '/',
+  './src/css/background.css',
+  './src/css/footer.css',
+  './src/css/card.css',
+  './src/css/body.css',
+  './src/css/menu.css',
+  './src/js/ios_splash.js',
+  './src/js/menu-mobile.js',
+  './src/js/main.js',
+];
+
+self.addEventListener('install', function (event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function (cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+
+  var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+cache.addAll(urlsToPrefetch.map(function(urlToPrefetch) {
+  return new Request(urlToPrefetch, { mode: 'no-cors' });
+})).then(function() {
+  console.log('All resources have been fetched and cached.');
+});
