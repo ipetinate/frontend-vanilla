f90e69e94e2a5a616668fd118e3d2ba724c31766
Isac Petinate <idpetinate@gmail.com>
2019-07-01 02:01:38 -0300

    Refactoring SW

--- sw.js
+++ sw.js
@@ -18,37 +18,37 @@ self.addEventListener('install', function (event) {
     caches.open(CACHE_NAME)
       .then(function (cache) {
         console.log('Opened cache');
-        return cache.addAll(urlsToPrefetch.map(function(urlToPrefetch) {
+        return cache.addAll(urlsToPrefetch.map(function (urlToPrefetch) {
           return new Request(urlToPrefetch, { mode: 'no-cors' });
-        })).then(function() {
+        })).then(function () {
           console.log('All resources have been fetched and cached.');
         });
       })
   );
 });
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   event.respondWith(
     caches.match(event.request)
-      .then(function(response) {
+      .then(function (response) {
         // Cache hit - return response
         if (response) {
           return response;
         }
         return fetch(event.request);
       }
-    )
+      )
   );
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
 
   var cacheWhitelist = ['pages-cache-v1', 'blog-posts-cache-v1'];
 
   event.waitUntil(
-    caches.keys().then(function(cacheNames) {
+    caches.keys().then(function (cacheNames) {
       return Promise.all(
-        cacheNames.map(function(cacheName) {
+        cacheNames.map(function (cacheName) {
           if (cacheWhitelist.indexOf(cacheName) === -1) {
             return caches.delete(cacheName);
           }
